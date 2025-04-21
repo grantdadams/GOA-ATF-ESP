65d42975269914a8e58eb53d85ad41ee2598afdb
Grant Adams <adamsgd@uw.edu>
2025-04-21 09:14:02 -0700

    Create Rceattle install.R

---
+++ Rceattle install.R
@@ -0,0 +1,24 @@
+
+# Install dependencies ----
+install.packages("pacman")
+install.packages("TMB", type = "source")
+install.packages("Matrix", type = "source")
+pacman::p_load(dplyr,
+               ggplot2,
+               MASS,
+               oce,
+               readxl,
+               TMB,
+               devtools,
+               writexl,
+               reshape2,
+               gplots,
+               tidyr,
+               testthat,
+               foreach,
+               R.utils,
+               rmarkdown,
+               knitr,
+               doParallel)
+devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
+remotes::install_github("grantdadams/Rceattle") # Master branch was the branch previously used for the ATF assessment
