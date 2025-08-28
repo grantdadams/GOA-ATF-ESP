c221b40fa66140bbf99edb087f01bd7e4938a33d
Grant Adams <adamsgd@uw.edu>
2025-08-28 09:12:02 -0700

    cleaning

--- R/Rceattle install.R
+++ R/Rceattle install.R
@@ -22,4 +22,7 @@ pacman::p_load(dplyr,
                knitr,
                doParallel)
 devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
+
+# NOTE: both DSEM and dev-name-change will work
 remotes::install_github("grantdadams/Rceattle", ref = "DSEM") # Master branch was the branch previously used for the ATF assessment
+remotes::install_github("grantdadams/Rceattle", ref = "dev-name-change") # Master branch was the branch previously used for the ATF assessment
