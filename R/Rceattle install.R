c88ce810c86c46310f28757e4200d8d9d692f220
Grant.Adams <grantadams60091@gmail.com>
2026-07-07 15:56:43 -0700

    Clean up install

--- R/Rceattle install.R
+++ R/Rceattle install.R
@@ -1,28 +1,8 @@
 
 # Install dependencies ----
-install.packages("pacman")
-install.packages("TMB", type = "source")
-install.packages("Matrix", type = "source")
-pacman::p_load(dplyr,
-               ggplot2,
-               MASS,
-               oce,
-               readxl,
-               TMB,
-               devtools,
-               writexl,
-               reshape2,
-               gplots,
-               tidyr,
-               remotes,
-               testthat,
-               foreach,
-               R.utils,
-               rmarkdown,
-               knitr,
-               doParallel)
-devtools::install_github("kaskr/TMB_contrib_R/TMBhelper")
+install.packages("remotes")
+remotes::install_github("grantdadams/Rceattle")
 
-# NOTE: both DSEM and dev-name-change will work, but use the dev-name-change for now
-remotes::install_github("grantdadams/Rceattle", ref = "dev-DSEM") # Master branch was the branch previously used for the ATF assessment
-remotes::install_github("grantdadams/Rceattle") # Master branch was the branch previously used for the ATF assessment
+# NOTE: both DSEM and main will work
+remotes::install_github("grantdadams/Rceattle", ref = "dev-DSEM")
+remotes::install_github("grantdadams/Rceattle")
